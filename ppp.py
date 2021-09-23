7c8391b50fbe37c22de053f6799abc93242664b6
haidarama <79550231+haidarama@users.noreply.github.com>
2021-09-23 15:48:21 +0700

    Update ppp.py

--- ppp.py
+++ ppp.py
@@ -1,32 +1,14 @@
+from tkinter import *
+root = Tk()
 
-import tkinter as tk
+a = Button(root, text="" ).grid(row=0, column=1)
+b = Button(root, text="" ).grid(row=0, column=2)
+c = Button(root, text="" ).grid(row=0, column=3)
+d = Button(root, text="" ).grid(row=1, column=1)
+e = Button(root, text="" ).grid(row=1, column=2)
+f = Button(root, text="" ).grid(row=1, column=3)
+g = Button(root, text="" ).grid(row=2, column=1)
+h = Button(root, text="" ).grid(row=2, column=2)
+i = Button(root, text="" ).grid(row=2, column=3)
 
-
-class Test():
-   def __init__(self):
-       self.root = tk.Tk()
-       self.root.geometry('200x100')
-       self.button = tk.Button(self.root,
-                          text = 'Click Me',
-                          command=lambda:[self.funcA(), self.funcB(), self.funcC()])
-       self.button.pack()
-
-       self.labelA = tk.Label(self.root, text="A")
-       self.labelB = tk.Label(self.root, text="B")
-       self.labelC = tk.Label(self.root, text="C")
-
-       self.labelA.pack()
-       self.labelB.pack()
-       self.labelC.pack()
-       
-       self.root.mainloop()
-
-   def funcA(self):
-       self.labelA["text"] = "A responds"
-
-   def funcB(self):
-       self.labelB["text"] = "B responds"
-   def funcC(self):
-       self.labelC["text"] = "C responds"
-       
-app = Test()
+root.mainloop()
