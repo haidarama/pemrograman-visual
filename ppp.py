a5fe6e9c8daee219b9b9c718bd4da5fcdf2098a1
haidarama <79550231+haidarama@users.noreply.github.com>
2021-09-26 10:03:43 +0700

    Update ppp.py

--- ppp.py
+++ ppp.py
@@ -1,14 +1,16 @@
 from tkinter import *
-root = Tk()
 
-a = Button(root, text="" ).grid(row=0, column=1)
-b = Button(root, text="" ).grid(row=0, column=2)
-c = Button(root, text="" ).grid(row=0, column=3)
-d = Button(root, text="" ).grid(row=1, column=1)
-e = Button(root, text="" ).grid(row=1, column=2)
-f = Button(root, text="" ).grid(row=1, column=3)
-g = Button(root, text="" ).grid(row=2, column=1)
-h = Button(root, text="" ).grid(row=2, column=2)
-i = Button(root, text="" ).grid(row=2, column=3)
+menu=Tk()
 
-root.mainloop()
+button = []
+for i in range(3):
+    m = 3+i
+    button.append(i)
+    button[i] = []
+    for j in range(3):
+        n = j
+        button[i].append(j)
+        button[i][j] = Button( bd=5, height=4, width=8)
+        button[i][j].grid(row=m, column=n)
+
+menu.mainloop()
